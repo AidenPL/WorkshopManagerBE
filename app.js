112a6fd40c6d20cc62af58b0791e02cdb8b399d1
AidenPL <aiden.lynch22@gmail.com>
2018-07-07 12:19:36 +0100

    Added Mongoose Connection

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app  = express();
 const bodyParser = require('body-parser');
 const mainRouter = require('./routes/apiRouter');
+const mongoose = require('mongoose');
+const dburl = require('./config').dburl
 
+mongoose.connect(dburl).then(() => {
+})
 
 app.use(bodyParser.json());
 
