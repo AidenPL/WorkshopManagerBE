51c55cedc3ae3b888b3e857f5d9a5f33674b0313
AidenPL <aiden.lynch22@gmail.com>
2018-07-15 13:15:41 +0100

    added Header Control

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const mainRouter = require('./routes/apiRouter');
 const mongoose = require('mongoose');
 const dburl = require('./config').dburl
 
-mongoose.connect(dburl).then(() => {
+mongoose.connect(dburl, { useNewUrlParser: true }).then(() => {
 })
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.use(bodyParser.json());
 
 app.use('/api', mainRouter);
